f7ca4feb451ba83b4c2fa298e4018a9a6339d74f
troymeeker <troymeeker1@gmail.com>
2021-07-12 15:32:52 -0700

    complete

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-// Write your solution in this file!
 const employee = {
-    name: "Sam",
-    streetAddress: "11 Broadway",
-
+  name:'Sam',
+  streetAddress: '11 Broadway'
 };
 
-function updateEmployeeWithKeyAndValue(employee, key, value ){
-  return Object.assign({},employee,{[key]:value});
+function updateEmployeeWithKeyAndValue(employee, key, value){
+ return Object.assign({},employee,{[key]:value})
 }
 
-
-function  destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress,value){
-   return Object.assign(employee,{[streetAddress]: "12 Broadway"});
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value){
+    return Object.assign(employee, {[streetAddress]:'12 Broadway'})
 }
 
-
-
-function deleteFromEmployeeByKey(employee,key,value) {
-   delete employee.key;
-    return Object.assign({},employee,{[key]:value});
-    
+function deleteFromEmployeeByKey(employee, key, value){
+    delete employee.key;
+    return Object.assign({},employee, {[key]:value});
 }
 
-
-function  destructivelyDeleteFromEmployeeByKey(employee, name){
-    delete employee.name;
-    return Object.assign(employee);
-
-      
-
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+ delete employee.name;
+ return Object.assign(employee);
 }
